60017f0e273cfdccdbb64487a64e93791214b4f2
puge <mail@puge.org>
2020-11-30 21:18:43 +0800

    save

--- app.js
+++ app.js
@@ -3,38 +3,4 @@ App({
   globalData: {
     openid: null
   },
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    wx.login({
-      success: (res) => {
-        if (res.code) {
-          console.log(res.code)
-          const userCode = res.code
-          //获取用户id
-          wx.request({
-            url: 'https://going.run/miniprogram',
-            data: {
-              type: 'login',
-              code: userCode
-            },
-            success: (res) => {
-              // console.log(res.data)
-              const data = JSON.parse(res.data)
-              this.globalData.openID = data['openID']
-              console.log(data['openID'])
-            },
-            fail: (res) => {
-              console.log(res)
-            }
-          })
-        } else {
-          console.log('登录失败！' + res.errMsg)
-        }
-      }
-    })
-  }
 })
